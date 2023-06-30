1ceb6d3f31980ea7fd60c47c253772353d3414ca
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2023-06-30 22:46:42 +0200

    feat: add more information to offers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest::Url;
 use rusty_money::iso::Currency;
+use time::OffsetDateTime;
 
 use crate::extractors::{Extract, Page};
 
@@ -7,14 +8,77 @@ use crate::extractors::{Extract, Page};
 mod extractors;
 
 #[derive(Debug)]
+enum StockInfo {
+  InStock(Option<u32>),
+  OutOfStock,
+}
+
+#[derive(Debug)]
+enum SaleVariant {
+  Regular,
+  LimitedTimeOffer { end_time: OffsetDateTime },
+  PreOrder { release_date: OffsetDateTime },
+  Bundle { items: Vec<String> },
+}
+
+#[derive(Debug)]
+enum Name {
+  Full(String),
+  FirstLast(String, String),
+}
+
+#[derive(Debug)]
+struct ContactInfo {
+  name: Option<Name>,
+  email: Option<String>,
+  phone: Option<String>,
+}
+
+#[derive(Debug)]
+enum BidStatus {
+  CurrentBidOnly((Price, Option<ContactInfo>)),
+  StartEndBids {
+    starting_bid: (Price, Option<ContactInfo>),
+    current_highest_bid: (Price, Option<ContactInfo>),
+  },
+  AllBids(Vec<(Price, Option<ContactInfo>)>),
+}
+
+#[derive(Debug)]
+struct SaleListing {
+  regular_price: Price,
+  sale_price: Option<Price>,
+  sale_variant: SaleVariant,
+}
+
+#[derive(Debug)]
+enum ListingType {
+  Sale(SaleListing),
+  Auction {
+    end_time: OffsetDateTime,
+    bid_status: BidStatus,
+  },
+}
+
+#[derive(Debug)]
+enum ItemState {
+  New,
+  Used,
+  Refurbished,
+  OpenBox,
+}
+
+#[derive(Debug)]
 struct Offer {
   title: String,
   link: Url,
-  price: Price,
-  is_auction: bool,
+  listing_type: ListingType,
   description: String,
   // use rustpostal later
+  item_state: Option<ItemState>,
+  stock_info: Option<StockInfo>,
   location: String,
+  sale_ended: bool,
 }
 
 #[derive(Debug)]
